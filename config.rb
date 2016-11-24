6be33c1b27c0a245739982a9e1203c9fe5cdcf26
Isaac Betesh <iybetesh@gmail.com>
2016-11-24 12:10:31 -0500

    Don't try to upload swp files

--- config.rb
+++ config.rb
@@ -8,6 +8,7 @@
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+ignore '*.swp'
 
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
